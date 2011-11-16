71173945908023c5a4dee3a1db94f514e9fae2ef
Andrew Kordik <hepaxcodex@gmail.com>
2011-11-15 20:01:39 -0500

    Started a Global-Share Branch Predictor

---
+++ CISCsim/GShareBranchPredictor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+    class GShareBranchPredictor
+    {
+        /// <summary>
+        /// Number of Predictor State machines
+        /// Defined by the Homework Specification
+        /// </summary>
+        private int numPredictorStateMachines = 512;
+
+        /// <summary>
+        /// Variable - Number of entries in the Branch Target Buffer
+        /// </summary>
+        public int NumBtbEntries;
+    }
+}
