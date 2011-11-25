18e593900157d4a38508c87c786feb7a7b0d3da3
Andrew Kordik <hepaxcodex@gmail.com>
2011-11-25 15:46:49 -0500

    Added a config Class

---
+++ CISCsim/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+    /// <summary>
+    /// Contains the Config details
+    /// </summary>
+    static class Config
+    {
+        public int superScalerFactor          = 5;
+        public int numReservationStations     = 10;
+        public int numFunctionalUnits         = 4; // Applies to everything except branch units
+        public int numRenamingTableEntries    = 10;
+        public int numReaderBufferEntries     = 10;
+
+    }
+}
