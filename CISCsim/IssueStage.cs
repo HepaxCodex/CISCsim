43528ad4a5a2cd5f9b952309e00740981bdb8616
Andrew Kordik <hepaxcodex@gmail.com>
2011-11-25 16:22:53 -0500

    Started the Issue stage

---
+++ CISCsim/IssueStage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+    class IssueStage
+    {
+        public ReservationStation integerStage;
+        public ReservationStation fpStage;
+        public ReservationStation memStage;
+        public ReservationStation multDivStage;
+        public ReservationStation branchStage;
+
+    }
+}
