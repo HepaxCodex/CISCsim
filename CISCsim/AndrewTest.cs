90875df1c12451d15186d068515ab3a41e111018
Andrew Kordik <hepaxcodex@gmail.com>
2011-11-25 15:29:00 -0500

    Added Some dummy static testing classes so things in Program.cs dont have conflicts

---
+++ CISCsim/AndrewTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+    static class AndrewTest
+    {
+        public static void RunTest()
+        {
+            Instruction foo = new Instruction("4204312 addiu r9,r9,-1");
+        }
+
+    }
+}
