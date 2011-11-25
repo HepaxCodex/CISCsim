813a8472e79a2200c178511840a41514c3d21b2c
Benjamin Fortener <fortenbt@gmail.com>
2011-11-25 16:32:17 -0500

    1. Removed unused file from project. 2. Added FetchStage class.

---
+++ CISCsim/FetchStage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+    /// <summary>
+    /// Defines the fetch stage.
+    /// Has an array of instructions called fetchBuffer.
+    /// </summary>
+    class FetchStage
+    {
+        public Instruction[] fetchBuffer;
+
+        public FetchStage()
+        {
+        }
+
+        public FetchStage(int superScalarWidth)
+        {
+            this.fetchBuffer = new Instruction[superScalarWidth];
+        }
+    }
+}
