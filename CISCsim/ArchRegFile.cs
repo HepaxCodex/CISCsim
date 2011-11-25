bcb3fc830527ceb4a6b4609d74a3127d41fedd86
Andrew Kordik <hepaxcodex@gmail.com>
2011-11-25 16:53:11 -0500

    Added Architechture register file or however the fuck you spell it

---
+++ CISCsim/ArchRegFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+    /// <summary>
+    /// Represents the Architecture Register File
+    /// </summary>
+    class ArchRegFile
+    {
+
+        /// <summary>
+        /// 0..31 == r0..r31
+        /// 32..62 == f0..f30
+        /// 63 == HiLo
+        /// 64 == FCC
+        /// </summary>
+        public ArfEntry[65] regFile;
+
+        public ArchRegFile()
+        {
+        }
+
+    }
+}
---
+++ CISCsim/ArfEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISCsim
+{
+
+    /// <summary>
+    /// Represents a Single Entry in the ARF
+    /// </summary>
+    class ArfEntry
+    {
+        /// <summary>
+        /// Actual Data Value
+        /// </summary>
+        public int data;
+
+        /// <summary>
+        /// If the current data is busy, it is NOT valid
+        /// </summary>
+        public bool busy;
+
+        /// <summary>
+        /// Entry in the Register Rename File 
+        /// </summary>
+        public int tag;
+
+    }
+}
